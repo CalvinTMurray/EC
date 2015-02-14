2d9dd167e47390f64c91187ddb162e53cbe27663
CszMurray <calvin.t.murray@hotmail.co.uk>
2015-02-14 22:02:41 +0000

    feat: add controller

--- angular-modules/main.js
+++ angular-modules/main.js
@@ -2,4 +2,8 @@
  * Created by Calvin . T . Murray on 14/02/2015.
  */
 
-angular.module('MainApp', ['ngMaterial']);
+angular.module('MainApp', ['ngMaterial'])
+
+.controller('ctrl', ['$scope', function($scope) {
+
+    }]);
